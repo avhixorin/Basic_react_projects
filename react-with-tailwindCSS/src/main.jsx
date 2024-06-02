ce81323bab20c0ce2db5b855cf726ddf2a58db6b
avhixorin <avhixorin@gmail.com>
2024-06-02 21:24:51 +0530

    props in react

--- react-with-tailwindCSS/src/main.jsx
+++ react-with-tailwindCSS/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card from './components/Card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <>
+
+      <App />
+      <Card userName="avhi" btnTxt="Click Me"/>
+      <Card userName="xorin" />
+
+    </>
+    
 )
