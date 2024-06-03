e39b52f412631c041a9c2419b4cf88a45e43076d
avhixorin <avhixorin@gmail.com>
2024-06-03 10:03:35 +0530

    .

--- bg_changer_project/src/App.jsx
+++ bg_changer_project/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +5,7 @@ function App() {
 
   return (
     <>
-  
+      <h1>Hello Ji</h1>
     </>
   )
 }
