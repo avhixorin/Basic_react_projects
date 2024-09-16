632a910cdead40d998ca3a70c4e9664bf2c76dc5
avhixorin <avhixorin@gmail.com>
2024-09-16 14:47:51 +0530

    Add Tailwind CSS configuration and styles

---
+++ reduxToolkitTodo/src/components/Comp1.jsx
@@ -0,0 +1,10 @@
+
+const Comp1 = () => {
+  return (
+    <div className="w-full min-h-screen bg-slate-700 flex justify-center items-center">
+      <p className="text-white text-3xl">Hii this is first div</p>
+    </div>
+  )
+}
+
+export default Comp1
---
+++ reduxToolkitTodo/src/components/Comp2.jsx
@@ -0,0 +1,10 @@
+
+const Comp2 = () => {
+  return (
+    <div className="w-full min-h-screen bg-slate-400 flex justify-center items-center">
+      <p className="text-white text-3xl">Hii this is second div</p>
+    </div>
+  )
+}
+
+export default Comp2
